e27c01d054247281b7e31888e74eecab67e5ec19
PatricksGH <p98.hartmann@icloud.com>
2026-02-14 21:26:56 +0100

    OLED implementiert  und getestet. Switch Pins geändert

--- keyboards/handwired/patrick/pmp_v1/keymaps/default/keymap.c
+++ keyboards/handwired/patrick/pmp_v1/keymaps/default/keymap.c
@@ -9,7 +9,6 @@
 
 #define RAW_HID_BUFFER_SIZE 32
 
-
 // Post Init Konfigurationen
 void keyboard_post_init_user(void) {
   analogReference(ADC_REF_POWER);
@@ -18,6 +17,13 @@ void keyboard_post_init_user(void) {
   //debug_keyboard=true;
   //debug_mouse=true;
 }
+
+
+// Globale Variablen
+static uint16_t last_time = 0;
+static uint8_t last_percent = 0;
+static uint8_t oled_percent = 0;
+static uint8_t last_oled_drawn = 255;
 
 
 // Keymaps definieren
@@ -29,10 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-// ADC lesen und per RAW-HID senden
-static uint16_t last_time = 0;
-static uint8_t last_percent = 0;
-
+// ADC lesen und periodisch per RAW-HID senden
 void matrix_scan_user(void) {
     if (timer_elapsed(last_time) < 100) return;
     last_time = timer_read();
@@ -40,6 +43,8 @@ void matrix_scan_user(void) {
     uint16_t adc_value = analogReadPin(ADC_PIN);
     uint32_t scaled = ((uint32_t)adc_value * 100 + 512) >> 10;
     uint8_t percent = (uint8_t)scaled;
+
+    oled_percent = percent;
 
     uprintf("ADC: %u%%\n", percent);
 
@@ -52,4 +57,25 @@ void matrix_scan_user(void) {
         raw_hid_send(buf, 32);
     }
 }
+
+
+// OLED-Display mit ADC-Wert aktualisieren
+bool oled_task_user(void) {
+    if (oled_percent == last_oled_drawn) {
+        return false; // nichts neu zeichnen
+    }
+    last_oled_drawn = oled_percent;
+
+    oled_clear();
+
+    oled_write_ln_P(PSTR("ADC VALUE"), false);
+    oled_write_ln_P(PSTR("--------"), false);
+
+    char buf[6];
+    snprintf(buf, sizeof(buf), "%3u", oled_percent);
+    oled_write_ln(buf, false);
+
+    return false;
+}
+
 
